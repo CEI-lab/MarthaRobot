3c77423b1394ef7de9a56f674389ad37aaf28d97
ParthSSharma <53370354+ParthSSharma@users.noreply.github.com>
2021-08-10 12:15:54 -0400

    Organized

--- commands/set-speed-command/SetSpeed.sh
+++
@@ -1,3 +0,0 @@
-#bin/bash
-sudo resources/SmcCmd/./SmcCmd -d $1 --speed $2 &
-sudo resources/SmcCmd/./SmcCmd -d $3 --speed $4 &
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PATH=$HOME/bin:$HOME/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-cd $HOME/HSI/
-export DISPLAY=:0
-export XAUTHORITY=/home/pi/.Xauthority
-echo "raspberry" | sudo -S python3 $HOME/HSI/HSIMaster.py #> $HOME/HSI/output 2>&1
