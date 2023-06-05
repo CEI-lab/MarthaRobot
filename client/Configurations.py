709f831acd44fee271196b5bf763ce14b83db741
bmanninen <bm586@cornell.edu>
2023-06-05 13:53:33 -0400

    added config file update function
    -allows any module that imports configurations to modify values
    - not locked/protected from multiple simultaneous edits

--- client/Configurations.py
+++ client/Configurations.py
@@ -5,7 +5,14 @@ Constants and variables for use throughout the project.  Includes GPIO pin numbe
 import logging
 import os
 
-home = os.path.expanduser('~pi')
+
+import tokenize
+from io import StringIO
+from collections import namedtuple
+import black
+
+
+home = os.path.expanduser("~pi")
 
 # CONFIGURATIONS = {
 # IMPORTANT : Image folder will always be in home directory with name 'Images'
@@ -23,7 +30,9 @@ NUMBER_OF_ALLOWED_FAILED_TCP_CONNECTIONS = 1000
 MAX_BYTES_OVER_TCP = 1024
 CHECK_NEW_IP_FROM_PI_FREQUENCY = 0.333
 RASPI_TXT_FILE_FULL_PATH_NAME = "{}/HSI/ip.txt"
-SENDING_IP_ADDRESS_TO_PI_COMMAND = "sudo -u pi scp -rp {} pi@{}:/home/pi/MEng/meng-hsi-group/tcp"
+SENDING_IP_ADDRESS_TO_PI_COMMAND = (
+    "sudo -u pi scp -rp {} pi@{}:/home/pi/MEng/meng-hsi-group/tcp"
+)
 SPEED_CONTROLLER_COMMAND = "{}/HSI/HSI/resources/SmcCmd/./SmcCmd"
 LEFT_WHEEL_SPEED_CONTROLLER_SERIAL_ID = "33FF-6D06-4D4B-3731-1919-1543"
 RIGHT_WHEEL_SPEED_CONTROLLER_SERIAL_ID = "52FF-7606-7283-5255-1917-2267"
@@ -67,7 +76,7 @@ M3_ADDRESS = 0xDE
 M1_SELECT = 0
 ENC1_3_PIN = 19
 ENC2_3_PIN = 6
-#bladder fan
+# bladder fan
 FAN_PIN = 21
 BLADDER_SIZE = [160, 160, 160]
 BLADDER_SPEED = 250
@@ -75,7 +84,7 @@ BLADDER_SPEED = 250
 
 
 # client.py
-HOST = '192.168.0.191'
+HOST = "192.168.0.191"
 COMMAND_PORT = 65432
 BYTES_PER_PACKET = 4096
 RESPONSE_PORT = 28200
@@ -83,8 +92,50 @@ RESPONSE_PORT = 28200
 
 # streaming_client.py
 STREAM_PORTS = {"rs": 1024, "tof": 1025, "ext": 1028}
-MC_IP_ADDRESS = '224.0.0.1'
+MC_IP_ADDRESS = "224.0.0.1"
 STREAM_CHUNK_SIZE = 4096
 
 # RPI server to get IPs
-RPI_SERVER = ''
+RPI_SERVER = 10
+
+
+def update(k: str, v: str):
+    collected = []
+    with open(__file__, "r") as f:
+        g = tokenize.generate_tokens(f.readline)
+        notfound = True
+        for token in g:
+            # print(token)
+            type, string, _, _, _ = token
+            collected.append((type, string))
+            if type == tokenize.NAME and string == k:
+                op = next(g)
+                optype, opstring, _, _, _ = op
+                val = next(g)
+                valtype, value, _, _, _ = val
+
+                if notfound and optype == tokenize.OP and opstring == "=":
+                    logging.info(f"Configuration {string} updated from {value} to {v}")
+                    collected.append((tokenize.OP, "="))
+                    notfound = False
+                    if valtype == tokenize.NUMBER:
+                        if str.isnumeric(v):
+                            collected.append((tokenize.NUMBER, str(v)))
+                        else:
+                            collected.append(value)
+                    elif valtype == tokenize.STRING:
+                        collected.append((tokenize.STRING, str(v)))
+
+                else:
+                    collected.append((optype, opstring))
+                    collected.append((valtype, value))
+
+    newSource = tokenize.untokenize(collected)
+    formated = black.format_str(newSource, mode=black.Mode())
+    with open(__file__, "w") as f:
+        f.write(formated)
+
+
+fileruns = 5
+
+update("fileruns", str(fileruns + 1))
