ec7668acc598fa38a840c648c4c8ad96256c413b
bmanninen <bm586@cornell.edu>
2023-06-08 14:30:58 -0400

    configuration variables can be documented
    - Does not work if variable name contains underscore

--- configurations/configurations.py
+++ configurations/configurations.py
@@ -17,40 +17,67 @@ home = os.path.expanduser("~pi")
 # Projector
 # IMPORTANT : Image folder will always be in home directory with name 'Images'
 IMAGES_DIRECTORY = "{}/Images/"
+""" Directory projector images will be stored in on the RPi """
 SHOW_DEFAULT_IMAGE = True
+""" 
+Whether to display an image on startup of the robot
+Not currently in use - TODO remove or implement
+"""
 DEFAULT_IMAGE_ON_STARTUP = "__0__.jpg"
-DEFAULT_IMAGE_DEFAULT_LOCATION = "{}/HSI/resources/defaults/"
-DEFAULT_IMAGE_OTHER_LOCATION = "{}/Images/"
+""" File address of the image that should be shown on startup """
+
 DISPLAY_IMAGE = None
+""" Variable to store the name of the current image being displayed """
 
 
 # TCP
 TCP_PORT = 65432
+""" Port to send TCP messages from computer -> rover """
 TCP_ENCODING = "utf-8"
+""" Encoding used for TCP """
 DEFAULT_RECEIVING_PORT = 28200
+""" Port to send TCP messages from rover -> computer """
 NUMBER_OF_ALLOWED_FAILED_TCP_CONNECTIONS = 1000
+""" Number of failed TCP connections before :func:`robot.tcp_manager.TCPManager._setServer` will fail """
 MAX_BYTES_OVER_TCP = 1024
+""" TCP packet size """
 
-
+# Misc connection
 RASPI_IP_ADDRESS = "199.168.1.212"
+""" The IP address of the rover's RPi """
 LOOP_BACK_IP_ADDRESS = "127.0.0.1"
+""" Loopback address for the RPi """
 CHECK_NEW_IP_FROM_PI_FREQUENCY = 0.333
+""" Frequency to check whether the rover's IP address has changed """
 RASPI_TXT_FILE_FULL_PATH_NAME = "{}/HSI/ip.txt"
+"""
+The file address on the 'server' RPi to store the IP address in
+Implemented, but not actively used since IPs in our setup remain constant for the most part
+"""
 SENDING_IP_ADDRESS_TO_PI_COMMAND = (
     "sudo -u pi scp -rp {} pi@{}:/home/pi/MEng/meng-hsi-group/tcp"
 )
+"""
+String representation of the command used to scp the IP file
+Might not currently be configured correctly, consider referencing :data:`RASPI_TXT_FILE_FULL_PATH_NAME`
+"""
 
 
 # Drive motors
 SPEED_CONTROLLER_COMMAND = "{}/HSI/HSI/resources/SmcCmd/./SmcCmd"
+"""
+File address to the command for interacting with the motor controller.
+These motor controllers do not have a python library so they are controlled by calling an external script.
+"""
 LEFT_WHEEL_SPEED_CONTROLLER_SERIAL_ID = "33FF-6D06-4D4B-3731-1919-1543"
+""" Serial ID to send commands to the left motor controller """
 RIGHT_WHEEL_SPEED_CONTROLLER_SERIAL_ID = "52FF-7606-7283-5255-1917-2267"
+""" Serial ID to send commands to the right motor controller """
 
 # Logging
 ENABLE_FILE_LOGGING = False
 LOG_FILENAME = "{}/hsi.log"  # Always in home directory
 LOGGING_LEVEL = logging.DEBUG  # For detailed logs change this to 'logging.DEBUG'
-NUMBER_OF_AIRCHANNEL_PAIRS = 3  # Number of air-channel pairs or systems.
 
 # Time of flight sensors
 TOF_PINS = [13, 26, 27]  # BCM [left middle right] (Possibly disable/enable pins)
@@ -76,6 +103,7 @@ M1_ADDRESS = 0x5E
 M1_SELECT = 1
 ENC1_1_PIN = 9
 ENC2_1_PIN = 25
+
 # bladder motor 2
 DIR1_2_PIN = 24
 DIR2_2_PIN = 15
@@ -83,6 +111,7 @@ M2_ADDRESS = 0x5E
 M1_SELECT = 0
 ENC1_2_PIN = 17
 ENC2_2_PIN = 18
+
 # bladder motor 3
 DIR1_3_PIN = 0
 DIR2_3_PIN = 22
@@ -90,18 +119,39 @@ M3_ADDRESS = 0xDE
 M1_SELECT = 0
 ENC1_3_PIN = 19
 ENC2_3_PIN = 6
+
 # bladder fan
 FAN_PIN = 21
+""" PWM pin to power the bladder fan."""
 BLADDER_SIZE = [160, 160, 160]
+""" Size of the bladder.  Should be a 3 int list representing the distance
+    each bladder motor can travel before the bladder reaches full inflation.
+    Asymetric bladders may have different values for each motor.
+"""
 BLADDER_SPEED = 250
-# }
+""" A default speed for the bladder motors. """
 
 
 # client.py
+# TODO combine TCP_PORT with COMMAND_PORT
 HOST = "192.168.0.191"
-COMMAND_PORT = 65432
+"""IP of the MarthaBot to connect to."""
+
+COMMANDPORT = 65432
+""" Port to send commands from computer -> rover
+
+    Should match :data:`~configurations.configurations.TCP_PORT`
+"""
 BYTES_PER_PACKET = 4096
+""" 
+Bytes per command packet.
+Might currently be incorrectly configured to be different from :data:`~configurations.configurations.MAX_BYTES_OVER_TCP`
+"""
 RESPONSE_PORT = 28200
+""" 
+Port to send commands from rover -> computer
+Should match :data:`~configurations.configurations.DEFAULT_RECIEVING_PORT`
+"""
 
 
 # streaming_client.py
@@ -113,7 +163,25 @@ STREAM_CHUNK_SIZE = 4096
 RPI_SERVER = 10
 
 
+# UNUSED
+# These seem to be unused TODO check that removing them is safe
+NUMBER_OF_AIRCHANNEL_PAIRS = 3  # Number of air-channel pairs or systems.
+DEFAULT_IMAGE_DEFAULT_LOCATION = "{}/HSI/resources/defaults/"
+DEFAULT_IMAGE_OTHER_LOCATION = "{}/Images/"
+
+
 def update(k: str, v: str):
+    """Modify the :mod:`configurations.configurations` source file to update the value assigned to a variable.
+
+    Implemented with two possible use cases in mind
+        - Modifying the client side file to match the currently connected robot's file
+        - Guided command line setup of a newly built robot
+
+    :param k: The name of the variable to be modified
+    :type k: str
+    :param v: The new value to be written
+    :type v: str
+    """
     collected = []
     with open(__file__, "r") as f:
         g = tokenize.generate_tokens(f.readline)
