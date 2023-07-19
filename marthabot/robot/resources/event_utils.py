ca16f4643f714f9b917d2d77f2952d387b2757ca
bm586 <bm586@cornell.edu>
2023-07-19 16:04:20 -0400

    event utils
    extend threading.event with ChildEvent, AndEvent, OrEvent
    
    Can wait on multiple events at the same time.

---
+++ marthabot/robot/resources/event_utils.py
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+import threading.Event
+
+class ChildEvent(threading.Event):
+  def __init__(self,parent = None):
+    super().__init()
+    self._parent = parent
+
+  def set_parent(self,parent):
+    self._parent = parent
+
+  def set(self):
+    super().set()
+    self._parent.update()
+
+  def clear(self):
+    super().clear()
+    self._parent.update()
+
+
+class AndEvent(threading.Event):
+    
+  def __init__(self, *events):
+    super().__init__()
+    self._children: list[ChildEvent] = list(events)
+    for event in self._children:
+      event.set_parent(self)
+    
+  def update(self):
+    bools = [e.is_set() for e in self._children]
+    if all(bools):
+      self.set()
+    else:
+      self.clear()
+
+  def addChild(self, child):
+    self._children.append(child)
+  
+class OrEvent(threading.Event):
+    
+  def __init__(self, *events):
+    super().__init__()
+    self._children: list[ChildEvent] = list(events)
+    for event in self._children:
+      event.set_parent(self)    
+      
+  def update(self):
+    bools = [e.is_set() for e in self._children]
+    if any(bools):
+      self.set()
+    else:
+      self.clear()
+
+  def addChild(self, child):
+    self._children.append(child)
+  
